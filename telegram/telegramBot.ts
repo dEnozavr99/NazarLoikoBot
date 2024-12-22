b920cb91197b02dcf7c6f2c9b92efd8067107748
Denys Rudyk <rudykdenys@gmail.com>
2024-12-22 13:51:13 +0200

    feat: add some custom types

--- telegram/telegramBot.ts
+++ telegram/telegramBot.ts
@@ -1,9 +1,13 @@
 import { Bot } from "grammy";
 
+enum TelegramBotCommands {
+  START = "start",
+}
+
 const createTelegramBot = (token: string): Bot => {
   const bot = new Bot(token);
 
-  bot.command("start", (context) =>
+  bot.command(TelegramBotCommands.START, (context) =>
     context.reply("Hello! I am a Nazar Loiko!")
   );
 
@@ -13,3 +17,26 @@ const createTelegramBot = (token: string): Bot => {
 };
 
 export { createTelegramBot };
+
+export class TelegramBotFactory {
+  private static bot: Bot;
+
+  private static addCommands(bot: Bot) {
+    bot.command("start", (context) =>
+      context.reply("Hello! I am a Nazar Loiko!")
+    );
+  }
+
+  private static addListeners(bot: Bot) {
+    bot.on("message", (context) => context.reply("Your mum :D"));
+  }
+
+  public buildBot(token: string): Bot {
+    const bot = new Bot(token);
+
+    TelegramBotFactory.addCommands(bot);
+    TelegramBotFactory.addListeners(bot);
+
+    return bot;
+  }
+}
